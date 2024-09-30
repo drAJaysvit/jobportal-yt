8a3c186a0f89d6916c186c6ad5c7cc30771bc8cc
drAJaysvit <109869929+drAJaysvit@users.noreply.github.com>
2024-09-30 21:52:29 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
 
 export const USER_API_END_POINT="https://jobportal-server-ybg3.onrender.com/api/v1/user";
 export const JOB_API_END_POINT="https://jobportal-server-ybg3.onrender.com/api/v1/job";
-export const APPLICATION_API_ED_POINT="https://jobportal-server-ybg3.onrender.com/api/v1/application";
+export const APPLICATION_API_END_POINT="https://jobportal-server-ybg3.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://jobportal-server-ybg3.onrender.com/api/v1/company";
