e18db1ab839119a1577ac4ec6bae49c3e2270a26
drAJaysvit <109869929+drAJaysvit@users.noreply.github.com>
2024-09-30 21:31:27 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const serverPath = "https://jobportal-server-ybg3.onrender.com";
+export const USER_API_END_POINT=`${serverPath}/api/v1/user`;
+export const JOB_API_END_POINT=`${serverPath}/api/v1/job`;
+export const APPLICATION_API_ED_POINT=`${serverPath}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${serverPath}/api/v1/company`;
